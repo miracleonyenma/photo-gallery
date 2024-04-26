96eba7bd5c12c4a2e99f2b7f7c9a1ab57880ac7a
miracleonyenma <miracleficient@gmail.com>
2024-04-26 22:06:20 +0100

    updates: Refactor usePhotoGallery hook to include saving and loading photos from storage

--- src/hooks/usePhotoGallery.ts
+++ src/hooks/usePhotoGallery.ts
@@ -1,13 +1,56 @@
-import { Camera, CameraResultType, CameraSource } from "@capacitor/camera";
-import { useState } from "react";
+import {
+  Camera,
+  CameraResultType,
+  CameraSource,
+  Photo,
+} from "@capacitor/camera";
+import { Directory, Filesystem } from "@capacitor/filesystem";
+import { Preferences } from "@capacitor/preferences";
+import { useEffect, useState } from "react";
 
 export interface UserPhoto {
   filepath: string;
   webviewPath?: string;
 }
 
+const PHOTO_STORAGE = "photos";
+
+export const base64FromPath = async (path: string): Promise<string> => {
+  const response = await fetch(path);
+  const blob = await response.blob();
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onerror = reject;
+    reader.onload = () => {
+      if (typeof reader.result === "string") {
+        resolve(reader.result);
+      } else {
+        reject("method did not return a string");
+      }
+    };
+    reader.readAsDataURL(blob);
+  });
+};
+
 const usePhotoGallery = () => {
   const [photos, setPhotos] = useState<UserPhoto[]>([]);
+
+  const savePicture = async (
+    photo: Photo,
+    fileName: string
+  ): Promise<UserPhoto> => {
+    const base64Data = await base64FromPath(photo.webPath!);
+    const savedFile = await Filesystem.writeFile({
+      path: fileName,
+      data: base64Data,
+      directory: Directory.Data,
+    });
+
+    return {
+      filepath: fileName,
+      webviewPath: photo.webPath,
+    };
+  };
 
   const takePhoto = async () => {
     const photo = await Camera.getPhoto({
@@ -17,19 +60,34 @@ const usePhotoGallery = () => {
       source: CameraSource.Camera,
     });
     const fileName = new Date().getTime() + ".jpeg";
-    const newPhotos = [
-      {
-        filepath: fileName,
-        webviewPath: photo.webPath,
-      },
-      ...photos,
-    ];
+    const savedFileImage = await savePicture(photo, fileName);
+    const newPhotos = [savedFileImage, ...photos];
     setPhotos(newPhotos);
+    Preferences.set({ key: PHOTO_STORAGE, value: JSON.stringify(newPhotos) });
   };
+
+  useEffect(() => {
+    const loadSaved = async () => {
+      const photosString = await Preferences.get({ key: PHOTO_STORAGE });
+      const photosInStorage = photosString.value
+        ? JSON.parse(photosString.value)
+        : [];
+      for (let photo of photosInStorage) {
+        const file = await Filesystem.readFile({
+          path: photo.filepath,
+          directory: Directory.Data,
+        });
+        photo.webviewPath = `data:image/jpeg;base64,${file.data}`;
+      }
+      setPhotos(photosInStorage);
+    };
+    loadSaved();
+  }, []);
 
   return {
     photos,
     takePhoto,
+    savePicture,
   };
 };
 
